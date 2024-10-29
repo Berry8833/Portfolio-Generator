216a8adf62570ee73bade35db9ab0848ea7a27db
Berry8833 <berryk8833@gmail.com>
2024-10-29 14:40:43 +0630

    2:40

---
+++ app.js
@@ -0,0 +1,63 @@
+// var message = "hello nodes";
+// var sum = 5 + 3;
+// console.log(message);
+// console.log(sum);
+
+
+// var commandLineArgs = process.argv;
+// console.log(comandLineArgs);
+
+// const profileDataArgs = process.argv.slice2, process.argv.length);
+// console.log(profile0dataArgs);
+
+// const message = "hello node";
+// message = "goodbye node";
+
+// const sum = 5 + 3;
+// sum +=1;
+
+// const animalArray = ['dog', 'cat', 'pig'];
+// animalArray.push{'cow'};
+
+// const personObj = {
+//     name: 'Zin Ko';
+//     age: 22
+// };
+// personObj.age = 32;
+// personObj.occupation = 'developer';
+
+// console.log(animalArray);
+// console.log(personObj);
+
+// let message = "hello node";
+// message ="hello ES6";
+// console.log(message);
+
+// let sum = 5 + 3;
+// sum += 1;
+// console.log(sum);
+
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+// };
+// printProfileData("Zin Ko");
+
+// const printProfileData = (profileDataArr, x) => {
+//     console.log(profileDataArr, x);
+// };
+// printProfileData("Zin Ko", "Developer");
+
+// const addNums = function(numOne, numTwo) {
+//     return numOne + numTwo;
+// };
+// console.log(addNums(4,3));
+
+// const addNums = (numOne, numTwo) => numOne + numTwo;
+
+// const sum = addNums(5,3);
+// console.log(sum);
+
+const addNums = (numOne, numTwo) => {
+    console.log(numOne, numTwo);
+    return numOne + numTwo;
+};
